000efcc568645a33ee2b7a3f75b3bbf319505a1a
sunxueb <sunxueb@163.com>
2018-10-08 14:08:04 +0000

    Updated database configuration 5

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 function get_db_config()
 {
     if(getenv('IS_IN_HEROKU')){
@@ -18,6 +18,29 @@ function get_db_config()
             'database'=>env('DB_DATABASE','forge'),
             'username'=>env('DB_USERNAME','forge'),
             'password'=>env('DB_PASSWORD',''),
+        ];
+    }
+}
+*/
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
         ];
     }
 }
