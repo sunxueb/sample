262e632316e2767150eb4fa9f456557d8888dd9c
sunxueb <sunxueb@163.com>
2018-10-08 14:03:31 +0000

    Updated database configuration4

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,10 +6,10 @@ function get_db_config()
         $url=parse_url(getenv("DATABASE_URL"));
         return $db_config=[
             'connection'=>'pgsql',
-            'host'=>$url['host'],
-            'database'=>substr($url['path'],1),
-            'username'=>$url('user'),
-            'password'=>$url['pass'],
+            'host'=>$url["host"],
+            'database'=>substr($url["path"],1),
+            'username'=>$url("user"),
+            'password'=>$url["pass"],
         ];
     }else{
         return $db_config=[
